42ff6fc4437aa6e37c55d417c28556177b9ddb40
peniel <eunicemodupe62@gmail.com>
2018-12-07 18:10:33 -0800

    Updated
    Changes made.

---
+++ dist/js/restaurant_info.js
@@ -0,0 +1,200 @@
+let restaurant;
+var newMap;
+
+/**
+ * Initialize map as soon as the page is loaded.
+ */
+document.addEventListener('DOMContentLoaded', (event) => {
+    initMap();
+});
+
+/**
+ * Initialize leaflet map
+ */
+initMap = () => {
+    fetchRestaurantFromURL((error, restaurant) => {
+        if (error) { // Got an error!
+            console.error(error);
+        } else {
+            self.newMap = L.map('map', {
+                center: [restaurant.latlng.lat, restaurant.latlng.lng],
+                zoom: 16,
+                scrollWheelZoom: false
+            });
+            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+                mapboxToken: 'pk.eyJ1IjoiZXVuaWNlbW9kdXBlIiwiYSI6ImNqbDE1MmxvaDE3emUza3Fyd3l2M3pmbXIifQ.E7PfcuFpvpf8YznFN9tESQ',
+                maxZoom: 18,
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                id: 'mapbox.streets'
+            }).addTo(newMap);
+            fillBreadcrumb();
+            DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
+        }
+    });
+}
+
+
+
+/**
+ * Get current restaurant from page URL.
+ */
+const fetchRestaurantFromURL = (callback) => {
+    if (self.restaurant) { // restaurant already fetched!
+        callback(null, self.restaurant)
+        return;
+    }
+    const id = getParameterByName('id');
+    if (!id) { // no id found in URL
+        let error = 'No restaurant id in URL'
+        callback(error, null);
+    } else {
+        DBHelper.fetchRestaurantById(id, (error, restaurant) => {
+            self.restaurant = restaurant;
+            if (!restaurant) {
+                console.error(error);
+                return;
+            }
+            fillRestaurantHTML();
+            callback(null, restaurant)
+        });
+    }
+}
+
+/**
+ * Create restaurant HTML and add it to the webpage
+ */
+const fillRestaurantHTML = (restaurant = self.restaurant) => {
+    const name = document.getElementById('restaurant-name');
+    name.innerHTML = restaurant.name;
+
+    const address = document.getElementById('restaurant-address');
+    address.innerHTML = restaurant.address;
+
+    const image = document.getElementById('restaurant-img');
+    image.className = 'restaurant-img';
+    image.alt = `${restaurant.name} restaurant's photo`;
+    image.src = DBHelper.imageUrlForRestaurant(restaurant);
+
+    // Set srcset for responsive
+    const image480 = image.src.replace(/(\.[\w\d_-]+)$/i, '-480$1')
+    image.setAttribute('srcset', `${image480} 480w, ${image.src} 800w`);
+
+    const cuisine = document.getElementById('restaurant-cuisine');
+    cuisine.innerHTML = restaurant.cuisine_type;
+
+    // fill operating hours
+    if (restaurant.operating_hours) {
+        fillRestaurantHoursHTML();
+    }
+    // fill reviews
+    fillReviewsHTML();
+}
+
+/**
+ * Create restaurant operating hours HTML table and add it to the webpage.
+ */
+const fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => {
+    const hours = document.getElementById('restaurant-hours');
+    for (let key in operatingHours) {
+        const row = document.createElement('tr');
+
+        const day = document.createElement('td');
+        day.innerHTML = key;
+        row.appendChild(day);
+
+        const time = document.createElement('td');
+        time.innerHTML = operatingHours[key];
+        row.appendChild(time);
+
+        hours.appendChild(row);
+    }
+}
+
+/**
+ * Create all reviews HTML and add them to the webpage.
+ */
+const fillReviewsHTML = (reviews = self.restaurant.reviews) => {
+    const container = document.getElementById('reviews-container');
+    const title = document.createElement('h3');
+    title.innerHTML = 'Reviews';
+    container.appendChild(title);
+
+    if (!reviews) {
+        const noReviews = document.createElement('p');
+        noReviews.innerHTML = 'No reviews yet!';
+        container.appendChild(noReviews);
+        return;
+    }
+    const ul = document.getElementById('reviews-list');
+    reviews.forEach(review => {
+        ul.appendChild(createReviewHTML(review));
+    });
+    container.appendChild(ul);
+}
+
+/**
+ * Create review HTML and add it to the webpage.
+ */
+const createReviewHTML = (review) => {
+    const li = document.createElement('li');
+    const reviewTitle = document.createElement('div');
+    reviewTitle.className = 'flex-container review-title';
+
+    const name = document.createElement('p');
+    name.innerHTML = review.name;
+    name.className = 'col';
+    reviewTitle.appendChild(name);
+
+    const date = document.createElement('p');
+    date.innerHTML = review.date;
+    date.className = 'col text-right';
+    reviewTitle.appendChild(date);
+
+    li.appendChild(reviewTitle);
+
+    const reviewContent = document.createElement('div');
+    reviewContent.className = 'review-content';
+
+    const rating = document.createElement('p');
+    rating.innerHTML = `Rating: ${review.rating}`;
+    rating.classList.add('badge');
+    reviewContent.appendChild(rating);
+
+    const comments = document.createElement('p');
+    comments.innerHTML = review.comments;
+    reviewContent.appendChild(comments);
+
+    li.appendChild(reviewContent);
+
+    return li;
+}
+
+/**
+ * Add restaurant name to the breadcrumb navigation menu
+ */
+const fillBreadcrumb = (restaurant = self.restaurant) => {
+    const breadcrumb = document.getElementById('breadcrumb');
+    const li = document.createElement('li');
+    li.innerHTML = restaurant.name;
+    li.setAttribute('aria-level', 2);
+    li.setAttribute('aria-current', 'page');
+    breadcrumb.appendChild(li);
+}
+
+/**
+ * Get a parameter by name from page URL.
+ */
+const getParameterByName = (name, url) => {
+    if (!url)
+        url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
+        results = regex.exec(url);
+    if (!results)
+        return null;
+    if (!results[2])
+        return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,88 +1,315 @@
-/*  eslint-env node */
+// var gulp = require('gulp');
+// var gulpLoadPlugins = require('gulp-load-plugins');
+// var fs = require('fs');
+// var del = require('del');
+// var browserify = require('browserify');
+// var babelify = require('babelify');
+// var source = require('vinyl-source-stream');
+// var buffer = require('vinyl-buffer');
+// var runSequence = require('run-sequence');
+// var lazypipe = require('lazypipe');
+// var browserSync = require('browser-sync').create();
 
-const gulp = require('gulp');
-const minify = require('gulp-clean-css');
-const sourcemaps = require('gulp-sourcemaps');
-const uglify = require('gulp-uglify');
-const babel = require('gulp-babel');
-const autoprifixer = require('gulp-autoprefixer');
-const imagemin = require('gulp-imagemin');
-const htmlmin = require('gulp-htmlmin');
-const concat = require('gulp-concat');
-const eslint = require('gulp-eslint');
-const pump = require('pump')
+// var $ = gulpLoadPlugins();
+// var reload = browserSync.reload;
 
-gulp.task('css', () => {
-  return gulp.src('./css/**/*.css')
-    .pipe(sourcemaps.init())
-    .pipe(autoprifixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(minify())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist/css/'))
+// // Lint JavaScript
+// gulp.task('lint', function() {
+//     return gulp.src(['app/**/*.js', '!node_modules/**'])
+//         .pipe($.eslint())
+//         .pipe($.eslint.format())
+//         .pipe($.eslint.failOnError());
+// });
+
+// // Build responsive images
+// gulp.task('images', ['fixed-images'], function() {
+//     return gulp.src('app/img/*.jpg')
+//         .pipe($.responsive({
+//             '*.jpg': [
+//                 { width: 300, rename: { suffix: '-300' }, },
+//                 { width: 400, rename: { suffix: '-400' }, },
+//                 { width: 600, rename: { suffix: '-600_2x' }, },
+//                 { width: 800, rename: { suffix: '-800_2x' }, }
+//             ]
+//         }, {
+//             quality: 40,
+//             progressive: true,
+//             withMetadata: false,
+//         }))
+//         .pipe(gulp.dest('.tmp/img'))
+//         .pipe(gulp.dest('dist/img'));
+// });
+
+// // Copy fixed images
+// gulp.task('fixed-images', function() {
+//     return gulp.src('app/img/fixed/**')
+//         .pipe(gulp.dest('.tmp/img/fixed'))
+//         .pipe(gulp.dest('dist/img/fixed'));
+// });
+
+// // Copy manifest
+// gulp.task('manifest', function() {
+//     return gulp.src('app/manifest.json')
+//         .pipe(gulp.dest('.tmp/'))
+//         .pipe(gulp.dest('dist/'));
+// });
+
+// // Prep assets for dev
+// gulp.task('html', function() {
+//     var apiKey = fs.readFileSync('AIzaSyDBttHLAubz7c0Pv78H1E2G56vHMw1u9ug', 'utf8');
+
+//     return gulp.src('app/*.html')
+//         .pipe($.stringReplace('AIzaSyDBttHLAubz7c0Pv78H1E2G56vHMw1u9ug', apiKey))
+//         .pipe($.useref())
+//         .pipe($.if('*.css', $.autoprefixer()))
+//         .pipe($.if('*.js', $.babel()))
+//         .pipe($.if('*.html', $.htmlmin({
+//             removeComments: true,
+//             collapseBooleanAttributes: true,
+//             removeAttributeQuotes: true,
+//             removeRedundantAttributes: true,
+//             removeEmptyAttributes: true,
+//             removeScriptTypeAttributes: true,
+//             removeStyleLinkTypeAttributes: true,
+//             removeOptionalTags: true
+//         })))
+
+//     .pipe(gulp.dest('.tmp'));
+// });
+
+// // Scan HTML for js & css and optimize them
+// gulp.task('html:dist', function() {
+//     var apiKey = fs.readFileSync('AIzaSyDBttHLAubz7c0Pv78H1E2G56vHMw1u9ug', 'utf8');
+
+//     return gulp.src('app/*.html')
+//         .pipe($.stringReplace('AIzaSyDBttHLAubz7c0Pv78H1E2G56vHMw1u9ug', apiKey))
+//         .pipe($.size({ title: 'html (before)' }))
+//         .pipe($.useref({},
+//             lazypipe().pipe($.sourcemaps.init)
+//             // lazypipe().pipe(babel) // no coz css
+//             // transforms assets before concat
+//         ))
+//         .pipe($.if('*.css', $.size({ title: 'styles (before)' })))
+//         .pipe($.if('*.css', $.cssnano()))
+//         .pipe($.if('*.css', $.size({ title: 'styles (after) ' })))
+//         .pipe($.if('*.css', $.autoprefixer()))
+//         .pipe($.if('*.js', $.babel()))
+//         .pipe($.if('*.js', $.size({ title: 'scripts (before)' })))
+//         .pipe($.if('*.js', $.uglifyEs.default()))
+//         .pipe($.if('*.js', $.size({ title: 'scripts (after) ' })))
+//         .pipe($.sourcemaps.write('.'))
+//         .pipe($.if('*.html', $.htmlmin({
+//             removeComments: true,
+//             collapseWhitespace: true,
+//             collapseBooleanAttributes: true,
+//             removeAttributeQuotes: true,
+//             removeRedundantAttributes: true,
+//             minifyJS: { compress: { drop_console: true } },
+//             removeEmptyAttributes: true,
+//             removeScriptTypeAttributes: true,
+//             removeStyleLinkTypeAttributes: true,
+//             removeOptionalTags: true
+//         })))
+
+//     .pipe($.if('*.html', $.size({ title: 'html (after) ', showFiles: false })))
+//         .pipe(gulp.dest('dist'));
+// });
+
+// // Process Service Worker
+// gulp.task('sw', function() {
+//     var bundler = browserify('./app/sw.js', { debug: true }); // ['1.js', '2.js']
+
+//     return bundler
+//         .transform(babelify, { sourceMaps: true }) // required for 'import'
+//         .bundle() // concat
+//         .pipe(source('sw.js')) // get text stream w/ destination filename
+//         .pipe(buffer()) // required to use stream w/ other plugins
+//         .pipe(gulp.dest('.tmp'));
+// });
+
+// // Optimize Service Worker
+// gulp.task('sw:dist', function() {
+//     var bundler = browserify('./app/sw.js', { debug: true }); // ['1.js', '2.js']
+
+//     return bundler
+//         .transform(babelify, { sourceMaps: true }) // required for 'import'
+//         .bundle() // concat
+//         .pipe(source('sw.js')) // get text stream w/ destination filename
+//         .pipe(buffer()) // required to use stream w/ other plugins
+//         .pipe($.size({ title: 'Service Worker (before)' }))
+//         .pipe($.sourcemaps.init({ loadMaps: true }))
+//         .pipe($.uglifyEs.default()) // minify
+//         .pipe($.size({ title: 'Service Worker (after) ' }))
+//         .pipe($.sourcemaps.write('./'))
+//         .pipe(gulp.dest('dist'));
+// });
+
+// // Clean temp directory
+// gulp.task('clean', function() {
+//     return del(['.tmp/**/*']); // del files rather than dirs to avoid error
+// });
+
+// // Clean output directory
+// gulp.task('clean:dist', function() {
+//     return del(['dist/**/*']); // del files rather than dirs to avoid error
+// });
+
+// // Watch files for changes & reload
+// gulp.task('serve', function() {
+//     runSequence(['clean'], ['images', 'lint', 'html', 'sw', 'manifest'], function() {
+//         browserSync.init({
+//             server: '.tmp',
+//             port: 8001
+//         });
+
+//         gulp.watch(['app/*.html'], ['html', reload]);
+//         gulp.watch(['app/css/*.css'], ['html', reload]);
+//         gulp.watch(['app/js/*.js'], ['lint', 'html', reload]);
+//         gulp.watch(['app/sw.js'], ['lint', 'sw', reload]);
+//         gulp.watch(['app/manifest.json'], ['manifest', reload]);
+//     });
+// });
+
+// // Build and serve the fully optimized site
+// gulp.task('serve:dist', ['default'], function() {
+//     browserSync.init({
+//         server: 'dist',
+//         port: 8000
+//     });
+
+//     gulp.watch(['app/*.html'], ['html:dist', reload]);
+//     gulp.watch(['app/css/*.css'], ['html:dist', reload]);
+//     gulp.watch(['app/js/*.js'], ['lint', 'html:dist', reload]);
+//     gulp.watch(['app/sw.js'], ['lint', 'sw', reload]);
+//     gulp.watch(['app/manifest.json'], ['manifest', reload]);
+// });
+
+// // Build production files, the default task
+// gulp.task('default', ['clean:dist'], function(done) {
+//     runSequence(['images', 'lint', 'html:dist', 'sw:dist', 'manifest'], done);
+// });
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var useref = require('gulp-useref');
+var gulpif = require('gulp-if');
+var compress = require('compression');
+
+var combineJS = [
+    'js/register_sw.js',
+    'js/dbhelper.js'
+];
+var individualJS = [
+    'js/indb-test/index.js',
+    'js/lib/lazyload.es2015.js',
+    'js/lib/lazyload.min.js',
+    'js/main.js',
+    'js/restaurant_info.js'
+];
+
+gulp.task('default', ['copy-html', 'copy-images', 'sw'], function() {
+    console.log('Done!');
 });
 
-gulp.task('scripts', function () {
-  gulp.src('./js/**/*.js').pipe(babel()).pipe(concat('all.js')).pipe(gulp.dest('./dist/js'))
-})
-
-gulp.task('js', function (cb) {
-  pump([ gulp.src('./js/**/*.js').pipe(sourcemaps.init()), uglify()
-    .pipe(sourcemaps.write('dist/sourcemaps')).pipe(gulp.dest('dist/js'))], cb)
+gulp.task('run', ['default', 'browserSync'], function() {
+    console.log('Starting the app');
 });
 
-gulp.task('idb', () => {
-  return gulp.src('./idb.js')
-  .pipe(uglify())
-  .pipe(gulp.dest('dist/'))
+//gulp.task('watch', ['browserSync', 'styles', 'copy-html', 'sw', 'scripts'], function(){
+gulp.task('watch', ['browserSync', 'styles', 'copy-html', 'sw'], function() {
+    gulp.watch('sass/**/*.scss', ['styles']);
+    gulp.watch('*.html', ['copy-html']);
+    gulp.watch('*.html', browserSync.reload);
+    gulp.watch('js/**/*.js', browserSync.reload);
 });
 
-gulp.task('index-html', () => {
-	return gulp.src('./index.html')
-  .pipe(htmlmin({ collapseWhitespace: true }))
-	.pipe(gulp.dest('dist/'))
+gulp.task('dist', [
+    'copy-html',
+    'copy-images',
+    'styles',
+    'scripts-dist'
+]);
+
+gulp.task('scripts', function() {
+    gulp.src('js/**/*.js')
+        // .pipe(babel({
+        //   presets: ['env']
+        // }))
+        //.pipe(concat('all.js'))
+        .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('restaurant-html', () => {
-  return gulp.src('./restaurant.html')
-  .pipe(htmlmin({ collapseWhitespace: true }))
-  .pipe(gulp.dest('dist/'))
+// gulp.task('scripts-dist', function(){
+//   gulp.src('js/**/*.js')
+//       .pipe(sourcemaps.init())
+//       .pipe(babel({
+//         presets: ['env']
+//       }))
+//       //.pipe(concat('all.js'))
+//       .pipe(uglify().on('error', e => console.log(e)))
+//       .pipe(sourcemaps.write())
+//       .pipe(gulp.dest('dist/js'));
+// });
+
+gulp.task('sw', function() {
+    gulp.src('./sw.js')
+        // .pipe(babel({
+        //   presets: ['env']
+        // }))
+        .pipe(gulp.dest('./dist'))
 });
 
-gulp.task('splash-html', () => {
-  return gulp.src('./splash.html')
-  .pipe(htmlmin({ collapseWhitespace: true }))
-  .pipe(gulp.dest('dist/'))
+gulp.task('copy-html', ['scripts', 'styles'], function() {
+    gulp.src(['./*.html', 'manifest.json'])
+        .pipe(useref())
+        // .pipe(gulpif('*.js', babel({ // added later
+        //   presets: ['env']
+        // })))
+        // .pipe(gulpif('*.js', uglify())) //minify js - added later
+        .pipe(gulp.dest('./dist'))
 });
 
-gulp.task('images', () => {
-	return gulp.src(['./img/*.jpg'])
-	.pipe(imagemin())
-	.pipe(gulp.dest('dist/img'))
+gulp.task('copy-images', function() {
+    gulp.src('img/**/*')
+        .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('manifest', () => {
-	return gulp.src(['./manifest.json'])
-	.pipe(gulp.dest('dist/'))
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(concat('combined.css'))
+        .pipe(gulp.dest('./dist/css'))
+        // .pipe(browserSync.reload({
+        //   stream: true
+        // }))
+    ;
 });
 
-gulp.task('lint', function () {
-  return gulp.src(['js/**/*.js'])
-    // eslint() attaches the lint output to the eslint property
-    // of the file object so it can be used by other modules.
-    .pipe(eslint())
-    // eslint.format() outputs the lint results to the console.
-    // Alternatively use eslint.formatEach() (see Docs).
-    .pipe(eslint.format())
-    // To have the process exit with an error code (1) on
-    // lint error, return the stream and pipe to failOnError last.
-    .pipe(eslint.failOnError());
+gulp.task('browserSync', function() {
+    browserSync.init({
+        port: 8000,
+        server: {
+            baseDir: "./dist"
+        },
+        middleware: [compress()]
+            // https: {
+            //   key: fs.readFileSync("ssl/filename.key", "utf8"),
+            //   cert: fs.readFileSync("ssl/filename.crt", "utf8")
+            //   //passphrase: 'password'
+            // },
+            // https: {
+            //   key: "ssl/2/server.key",
+            //   cert: "ssl/2/server.crt"
+            //   //passphrase: 'password'
+            // }
+    });
 });
-
-gulp.task('watch', () => {
- gulp.watch(['./*.html', './**/*.js', './css/*.css', './manifest.json'], 'default')
-});
-
-gulp.task('default', ['js', 'css', 'html', 'splash-html', 'idb', 'scripts', 'lint', 'restaurant-html', 'index-html', 'manifest', 'images', 'watch']);
